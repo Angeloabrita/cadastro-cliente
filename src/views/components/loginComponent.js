4f3024af569b30cd7a9ec477e808bcc3df466875
agta <angeloabrita@gmail.com>
2024-06-19 22:43:38 -0300

    :lipstick: feat: atualizand style

--- src/views/components/loginComponent.js
+++ src/views/components/loginComponent.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import AuthService from '../../services/authServices';
 import { useNavigate } from 'react-router-dom';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 function Login() {
   const [username, setUsername] = useState('');
@@ -10,31 +11,45 @@ function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-
-     await AuthService.login(username, password)
-     navigate('dashboard');
-
+      await AuthService.login(username, password);
+      navigate('dashboard');
     } catch (error) {
       alert('Ops! Ocorreu um erro ao logar, atualize a pagina e tente novamente');
     }
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Login</button>
-    </form>
+    <Container className='d-flex justify-content-center align-items-center vh-100'>
+      <Row className="justify-content-md-center ">
+        <Col md={12}>
+          <Form onSubmit={handleLogin} className="p-4 bg-light  rounded">
+            <Form.Group controlId="formUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formPassword" className="mt-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" className="mt-4">
+              Login
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
