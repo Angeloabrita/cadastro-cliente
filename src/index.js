7cb808b71a22201cb0c9932ff9fa690577eee8c0
agta <angeloabrita@gmail.com>
2024-06-21 00:19:46 -0300

    :recycle: refactor: implementando gh-page

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
